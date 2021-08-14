4cd7be32d7a9845b476b24fddac5a2253be066b1
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-08-15 01:39:15 +0530

    Delete option Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,67 @@
 import React, { useState } from "react";
-import TodoLists  from "./TodoLists";
+import TodoLists from "./TodoLists";
 
 const App = () => {
   const [inputList, setInputList] = useState("");
   const [Items, setItems] = useState([]);
 
   const itemEvent = (event) => {
-      setInputList(event.target.value);
+    setInputList(event.target.value);
   };
 
   const listOfItem = () => {
-    setItems((oldItems)=>{
-      return [...oldItems,inputList];
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
     });
     setInputList("");
+  };
+
+  const deleteItem = (id) => {
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElement,index)=>{
+        return index != id;
+      })
+    })
+
+
+
   };
 
   return (
     <div className="wrapper">
       <header>Todo App</header>
       <div className="inputField">
-        <input type="text" placeholder="Add your new todo" value={inputList} onChange={itemEvent}/>
-        <button onClick={listOfItem} ><i className="fas fa-plus" /></button>
+        <input
+          type="text"
+          placeholder="Add your new todo"
+          value={inputList}
+          onChange={itemEvent}
+        />
+        <button onClick={listOfItem}>
+          <i className="fas fa-plus" />
+        </button>
       </div>
 
-      <ul className="todoList" >
-     { Items.map((itemval) => {
-        return <TodoLists />
-      })
-    }
-      </ul >
+      <ul className="todoList">
+        {Items.map((itemval, index) => {
+          return (
+            <TodoLists
+              key={index}
+              id={index}
+              text={itemval}
+              onSelect={deleteItem}
+            />
+          );
+        })}
+      </ul>
       <div className="footer">
-      <span>You have <span className="pendingTasks"></span> pending tasks</span>
-        <button >Clear All</button>
-        
+        <span>
+          You have <span className="pendingTasks"></span> pending tasks
+        </span>
+        <button>Clear All</button>
+      </div>
     </div>
-    </div>
- )
- 
+  );
 };
 
 export default App;
--- src/TodoLists.jsx
+++ src/TodoLists.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const TodoLists = (props) => {
+
   return (
     <div className="todo_list">
-      <i className="fa fa-times" aria-hidden="true"></i>
+      <i className="fa fa-times" aria-hidden="true"
+       onClick={()=>{
+           props.onSelect(props.id);
+       }} />
       <li> {props.text} </li>
     </div>
   );
